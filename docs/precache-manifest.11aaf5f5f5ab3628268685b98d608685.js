f55c7c1b35dc3a72c702dec8c5f92988624a1473
Hector Alvarez <halain80@gmail.com>
2020-08-24 23:10:23 -0500

    rename build to docs for github pages

---
+++ docs/precache-manifest.11aaf5f5f5ab3628268685b98d608685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6527131816cace31e3eac26c4ee3e5c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4e371af32d458c1564f",
+    "url": "/static/css/main.5d49a2aa.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e371af32d458c1564f",
+    "url": "/static/js/main.6e21fe5b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
